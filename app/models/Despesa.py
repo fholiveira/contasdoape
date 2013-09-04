d5ee20c34a31098872f747ff16ef625494233768
Fernando Oliveira <fernando@fholiveira.com>
2013-09-04 14:23:28 -0300

    Escrevendo testes de inicialização

--- app/models/Despesa.py
+++ app/models/Despesa.py
@@ -5,3 +5,16 @@ class Despesa(Document):
     descricao = StringField(required = True)
     valor = FloatField(required = True)
     data = DateTimeField(required = True)
+
+    def __init__(self, valor, nome, data):
+        self._validar_parametros_de_construcao(valor, nome)
+
+        self.nome = nome
+        self.valor = valor
+        
+    def _validar_parametros_de_construcao(self, valor, nome):
+        if not valor:
+            raise ValueError("valor")
+
+        if not nome:
+            raise ValueError(nome)
--- app/tests/TestDespesa.py
+++ app/tests/TestDespesa.py
@@ -1,10 +1,11 @@
 from unittest import TestCase
 from models.Despesa import Despesa
+from datetime import datetime
 
 class TestDespesa(TestCase):
 
     def setUp(self):
-        self.despesa = Despesa()
+        self.despesa = Despesa(valor = 1, nome = "Ususario", data = datetime.now)
     
     def test_deve_conter_descricao(self):
         self.assertTrue(hasattr(self.despesa, "descricao"))
@@ -17,3 +18,9 @@ class TestDespesa(TestCase):
 
     def test_deve_conter_data(self):
         self.assertTrue(hasattr(self.despesa, "data"))
+
+    def test_nao_deve_existir_sem_valor_positivo(self):
+        self.assertRaises(ValueError, Despesa(valor = 0))
+
+    def test_nao_deve_existir_sem_nome(self):
+        self.assertRaises(ValueError, Despesa(valor = 2, nome = None))
