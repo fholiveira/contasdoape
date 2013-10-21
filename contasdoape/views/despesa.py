23d4ebb3fdcf8366ed5d2a1a58cc3b177e1f5342
Fernando Oliveira <fernando@fholiveira.com>
2013-10-21 13:31:52 -0200

    REfatorando a view de despesas

--- contasdoape/views/despesa.py
+++ contasdoape/views/despesa.py
@@ -1,25 +1,24 @@
-from bottle import TEMPLATE_PATH, get, post, redirect, request, jinja2_template as template
-from app.models.Despesa import Despesa
-from app.models.Caixa import Caixa
-from app.models.Tesoureiro import Tesoureiro
+from flask import render_template, request
+from contasdoape import app
+from models.Despesa import Despesa
+from models.Caixa import Caixa
+from models.Tesoureiro import Tesoureiro
 from datetime import datetime
 
-TEMPLATE_PATH.append('./templates')
-
-@get('/despesas/nova')
+@app.route('/despesas/nova', methods=['GET'])
 def home():
-    return template('novadespesa.html')
+    return render_template('novadespesa.html')
 
-@post('/despesas/nova')
+@app.route('/despesas/nova', methods=['POST'])
 def nova_despesa():
-    Caixa().lancar_despesa(data = request.POST.data,
-                           autor = request.POST.autor,
-                           valor = request.POST.valor,
-                           descricao = request.POST.descricao)
+    Caixa().lancar_despesa(data = request.form.data,
+                           autor = request.form.autor,
+                           valor = request.form.valor,
+                           descricao = request.form.descricao)
     
     redirect('/')
 
-@get('/')
+@app.route('/', methods=['GET'])
 def listar_despesas():
     ano = request.query.ano or datetime.now().year
     mes = request.query.mes or datetime.now().month
@@ -27,9 +26,9 @@ def listar_despesas():
     mes_fiscal = Tesoureiro().obter_mes_fiscal(datetime(year=int(ano), month=int(mes), day=1))
     despesas = mes_fiscal.listar_despesas()
 
-    return template('despesas.html',
-                    despesas = [d.to_dict() for d in despesas], 
-                    data_inicio = mes_fiscal.data_inicio.strftime('%d/%m/%Y'), 
-                    data_fim = mes_fiscal.data_fim.strftime('%d/%m/%Y'),
-                    mes = mes_fiscal.nome_do_mes() + ' de ' + str(mes_fiscal.data_inicio.year),
-                    total = mes_fiscal.calcular_saldo())
+    return render_template('despesas.html',
+                           despesas = [d.to_dict() for d in despesas], 
+                           data_inicio = mes_fiscal.data_inicio.strftime('%d/%m/%Y'), 
+                           data_fim = mes_fiscal.data_fim.strftime('%d/%m/%Y'),
+                           mes = mes_fiscal.nome_do_mes() + ' de ' + str(mes_fiscal.data_inicio.year),
+                           total = mes_fiscal.calcular_saldo())
--- contasdoape/views/usuario.py
+++
@@ -1,6 +0,0 @@
-from bottle import TEMPLATE_PATH, get, post, redirect, request, jinja2_template as template
-
-@get('/usuario')
-def mostrar_usuario():
-    return template('usuario.html')
-
