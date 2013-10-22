c219aeb1e54298f43eca99e94124e3e1badb4d3c
Fernando Oliveira <fernando@fholiveira.com>
2013-10-22 21:31:39 -0200

    Fazendo com que o flask enxergue a view de sumario

--- contasdoape/__init__.py
+++ contasdoape/__init__.py
@@ -4,4 +4,5 @@ from mongoengine import connect
 app = Flask(__name__)
 connect('contasdoape')
 
-from .views  import despesa
+from contasdoape.views import despesa
+from contasdoape.views import sumario 
