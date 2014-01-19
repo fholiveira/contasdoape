0dfd98360598b0480811e1186056ac44d97e9c60
Fernando Oliveira <fernando@fholiveira.com>
2014-01-18 23:40:45 -0200

    Teste de integração sobre adicionar convidados

--- testes/integracao/TestApe.py
+++ testes/integracao/TestApe.py
@@ -5,33 +5,43 @@ from contasdoape.models.Condominio import Condominio
 from contasdoape.models.ControleDeAcesso import ControleDeAcesso
 
 class TestApe(TestCase):
+   
+    @classmethod
+    def setUpClass(cls):
+        cls.conexao = connect('contasdoape-test')
+        cls.usuario = ControleDeAcesso().obter_usuario('123456', 'João')
+
     def setUp(self):
-        self.conexao = connect('contasdoape-test')
-        self.usuario = ControleDeAcesso().obter_usuario('123456', 'João')
+        Ape.objects.delete()
 
     def test_deve_criar_apartamento(self):
-        Ape.objects.delete()
-        
         Condominio().criar_ape(self.usuario)
         ape = Ape.objects().first()
         
         self.assertIsNotNone(ape)
 
     def test_deve_obter_ape(self):
-        Ape.objects.delete()
-
         novo_ape = Condominio().criar_ape(self.usuario)
         ape = Condominio().obter_ape(self.usuario)
 
         self.assertEquals(novo_ape.id, ape.id)
     
     def test_deve_associar_usuario_ao_criar_o_ape(self):
-        Ape.objects.delete()
-        
         Condominio().criar_ape(self.usuario)
         ape = Ape.objects().first()
         
         self.assertEquals(self.usuario.id, ape.membros[0].id)
 
-    def tearDown(self):
-       self.conexao.drop_database('contasdoape-test')
+
+    def test_deve_convidar_amigos(self):
+        ape = Condominio().criar_ape(self.usuario)
+        convidados = [ '111111', '222222' ]
+
+        ape.adicionar_convidados(convidados)
+
+        ape_salvo = Ape.objects.first()
+        self.assertListEqual(ape_salvo.convidados, convidados)
+
+    @classmethod
+    def tearDownClass(cls):
+       cls.conexao.drop_database('contasdoape-test')
