82674514c9474160e31b12c40983b2a2ca722c37
Fernando Oliveira <fernando@fholiveira.com>
2014-02-19 15:21:48 -0300

    Adicionando usuarios de teste

---
+++ features/usuarios.py
@@ -0,0 +1,26 @@
+usuarios = {
+    'Rorschach': { 
+        'nome': 'Walter Kovacs', 
+        'email': 'email do usuario no facebook', 
+        'facebook': 'id do facebook', 
+        'senha': 'senha do facebook' 
+    },
+    'Comediante': { 
+        'nome': 'Edward Blake', 
+        'email': 'email do usuario no facebook', 
+        'facebook': 'id do facebook', 
+        'senha': 'senha do facebook' 
+    },
+    'Coruja': { 
+        'nome': 'Dan Dreiberg', 
+        'email': 'email do usuario no facebook', 
+        'facebook': 'id do facebook', 
+        'senha': 'senha do facebook' 
+    },
+    'Ozymandias': { 
+        'nome': 'Adrian Veidt', 
+        'email': 'email do usuario no facebook', 
+        'facebook': 'id do facebook', 
+        'senha': 'senha do facebook' 
+    }
+}
