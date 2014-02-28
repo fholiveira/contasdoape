464cb688e3ecc2bb0d5803b95c01f75f4d377d84
Fernando Oliveira <fernando@fholiveira.com>
2014-02-28 09:24:14 -0300

    Alterando local do arquivo de configuração

--- contasdoape/web.py
+++ contasdoape/web.py
@@ -4,18 +4,9 @@ from flask.ext.login import LoginManager
 from os import path, getcwd, environ, listdir
 from contasdoape.environments import Environments
 
-def load_envs():
-    diretorio = 'contasdoape/config'
-    env = Environments(app, var_name='CONTASDOAPE_ENV')
-    arquivos = [arquivo for arquivo in listdir(diretorio)
-                if path.isfile(path.join(diretorio, arquivo))]
-
-    for arquivo in arquivos:
-        env.from_yaml(path.join(diretorio, arquivo))
-
 app = Flask(__name__)
 
-load_envs()
+Environments(app, var_name='CONTASDOAPE_ENV').from_yaml('contasdoape/conf.yaml')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
